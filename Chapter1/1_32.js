b69a112e3c64890c87870bd4448be8580a89077a
HyoSeong <mongsiry013@naver.com>
2023-01-23 20:34:37 +0900

    1_32

--- Chapter1/1_32.js
+++ Chapter1/1_32.js
@@ -17,3 +17,24 @@ function product(term, a, next, b) {
     }
     return accumulate(combiner, 1, term, a, next, b)
 }
+
+function accumulate_iter(combiner, null_value, term, a, next, b) {
+    function iter(a, result) {
+        return a > b ? result : iter(next(a), combiner(term(a), result))
+    }
+    return iter(a, null_value)
+}
+
+function sum_iter(term, a, next, b) {
+    function combiner(a, b) {
+        return a + b
+    }
+    return accumulate_iter(combiner, 0, term, a, next, b)
+}
+
+function product_iter(term, a, next, b) {
+    function combiner(a, b) {
+        return a * b
+    }
+    return accumulate_iter(combiner, 1, term, a, next, b)
+}
