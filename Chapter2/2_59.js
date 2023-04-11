a46740371f39aa618c289ca9b2d40d2e03dd2fe8
HyoSeong <mongsiry013@naver.com>
2023-04-12 06:50:32 +0900

    2_59

---
+++ Chapter2/2_59.js
@@ -0,0 +1,29 @@
+function is_element_of_set(x, set) {
+    return is_null(set) 
+           ? false
+           : equal(x, head(set))
+           ? true
+           : is_element_of_set(x, tail(set));
+}
+
+function adjoin_set(x, set) {
+    return is_element_of_set(x, set)
+           ? set
+           : pair(x, set);
+}
+
+function intersection_set(set1, set2) {
+    return is_null(set1) || is_null(set2)
+           ? null
+           : is_element_of_set(head(set1), set2)
+           ? pair(head(set1), intersection_set(tail(set1), set2))
+           : intersection_set(tail(set1), set2);
+}
+
+function union_set(set1, set2) {
+    return is_null(set1)
+           ? set2
+           : is_element_of_set(head(set1), set2)
+           ? union_set(tail(set1), set2)
+           : pair(head(set1), union_set(tail(set1), set2));
+}
