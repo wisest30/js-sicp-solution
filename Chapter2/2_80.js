ceb3a02a69777c966de36c3a54c88a170105017a
HyoSeong <mongsiry013@naver.com>
2023-05-22 21:42:45 +0900

    2_80

---
+++ Chapter2/2_80.js
@@ -0,0 +1,6 @@
+function is_equal_to_zero(x, y) {
+    return apply_generic("is_equal_to_zero", list(x, y));
+}
+
+// 각 package 별 is_equal 함수 구현. (코드 생략)
+// 각 package 별 install 함수에 put() 함수 실행. (코드 생략)
